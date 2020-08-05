89020bb4e52b4d1af1296ed90fa81f700c028aeb
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-08-05 15:17:13 +0200

    Small refactor

--- server.c
+++ server.c
@@ -12,13 +12,13 @@
 #include "http.h"
 
 #define PORT 8080
-#define SA struct sockaddr 
 
 #define handle_error(msg) \
     do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 
-char *find_type(char *buff) {
+char *find_type(char *buff)
+{
     char *type;
     char *temp;
 
@@ -30,9 +30,9 @@ char *find_type(char *buff) {
 
     printf("Found type: %s\n", type);
 
-    if(!strcmp(type, "jpg")) {
+    if (!strcmp(type, "jpg")) {
         return "image/jpg";
-    } else if(!strcmp(type, "html")) {
+    } else if (!strcmp(type, "html")) {
         return "text/html";
     }
     
@@ -40,64 +40,63 @@ char *find_type(char *buff) {
 }
 
 // Driver function 
-int main() 
-{ 
-    int sockfd, connfd, len; 
-    struct sockaddr_in servaddr, cli; 
+int main() {
+    int sockfd, connfd, len;
+    struct sockaddr_in servaddr, cli;
     char response[4096];
 
-    // socket create and verification 
+    // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
         handle_error("setsockopt(SO_REUSEADDR) failed");
 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    } else {
+        printf("Socket successfully created..\n");
+    }
+    memset(&servaddr, 0, sizeof(struct sockaddr_in));
   
     // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
   
     // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+    if ((bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
         handle_error("Bind failed");
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully binded..\n"); 
+        exit(0);
+    } else {
+        printf("Socket successfully binded..\n");
+    }
   
     // Now server is ready to listen and verification 
-    if ((listen(sockfd, 5)) != 0) { 
-        printf("Listen failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Server listening..\n"); 
-    len = sizeof(cli); 
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listen failed...\n");
+        exit(0);
+    } else {
+        printf("Server listening..\n");
+    }
+    len = sizeof(cli);
   
     // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (SA*)&cli, &len); 
-    if (connfd < 0) { 
-        printf("server acccept failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("server acccept the client...\n"); 
-  
+    connfd = accept(sockfd, (struct sockaddr *)&cli, (socklen_t *)&len);
+    if (connfd < 0) {
+        printf("server acccept failed...\n");
+        exit(0);
+    } else {
+        printf("server acccept the client...\n");
+    }
+
     memset(response,0,sizeof(response));
 
     while(1) {
 
         int n;
         n = read(connfd, response, 4096);
-        if(n < 0) {
+        if (n < 0) {
             printf("Error reading\n");
         } else {
             printf("Recieved:\n%s", response);
@@ -117,7 +116,7 @@ int main()
 
         printf("\n*****************\nRequested resource: %s\n", resource);
 
-        if(!strcmp(resource, "/")) {
+        if (!strcmp(resource, "/")) {
             strcpy(filename, "index.html");
         } else {
             strcpy(requestedResource, resource + 1);
@@ -125,7 +124,7 @@ int main()
         }
 
         int fd = open(filename, O_RDONLY);
-        if(fd == -1) {
+        if (fd == -1) {
             fd = open("404.html", O_RDONLY);
             code = 404;
             strcpy(message, "Not Found");
@@ -145,7 +144,7 @@ int main()
             int a = sendfile(connfd, fd, NULL, stat_buf.st_size);
             printf("\n%d\n*******************\n", a);
             close(fd);
-        
+
     }
 
     close(connfd);
